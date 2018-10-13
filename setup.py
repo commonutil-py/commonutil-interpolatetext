0e809527a959f6c38f6be48a42b4db802f936ab2
Cheyin L <cheyinl@gmail.com>
2018-10-14 02:38:22 +0800

    bump revision

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup  # pylint: disable=import-error
 
 setup(
 		name="commonutil-interpolatetext",
-		version="0.0.1",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
+		version="0.1.0",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
 		description="Interpolate text template engine",
 		packages=[
 				"commonutil_interpolatetext",
